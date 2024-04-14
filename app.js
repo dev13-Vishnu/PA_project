0aa3c375b2f13c611282e96aaac5652df2a72eaa
Vishnu P S <vishnu13three@gamil.com>
2024-04-14 17:43:21 +0530

    changed function replaceHtml to a module of same name

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const readline = require('readline');
 const url = require('url');
+const replaceHtml = require('./modules/replaceHtml')
 
 const html = fs.readFileSync('./template/index.html','utf-8');
 let products = JSON.parse(fs.readFileSync('./data/products.json','utf-8'));
@@ -9,21 +10,21 @@ let productListHtml = fs.readFileSync('./template/product-list.html','utf-8');
 let productDetailtHtml = fs.readFileSync('./template/product-details.html','utf-8');
 
 
-function replaceHtml (template,product) {
-        let output = template.replace('{{%IMAGE%}}',product.productImage);
-        output= output.replace('{{%NAME%}}',product.name);
-        output= output.replace('{{%MODELNAME%}}',product.modeName);
-        output= output.replace('{{%MODELNO%}}',product.modelNumber);
-        output= output.replace('{{%SIZE%}}',product.size);
-        output= output.replace('{{%CAMERA%}}',product.camera);
-        output= output.replace('{{%PRICES%}}',product.price);
-        output= output.replace('{{%COLOR%}}',product.color);
-        output= output.replace('{{%ID%}}',product.id);
-        output= output.replace('{{%ROM%}}',product.ROM);
-        output= output.replace('{{%DESC%}}',product.Description);
+// function replaceHtml (template,product) {
+//         let output = template.replace('{{%IMAGE%}}',product.productImage);
+//         output= output.replace('{{%NAME%}}',product.name);
+//         output= output.replace('{{%MODELNAME%}}',product.modeName);
+//         output= output.replace('{{%MODELNO%}}',product.modelNumber);
+//         output= output.replace('{{%SIZE%}}',product.size);
+//         output= output.replace('{{%CAMERA%}}',product.camera);
+//         output= output.replace('{{%PRICES%}}',product.price);
+//         output= output.replace('{{%COLOR%}}',product.color);
+//         output= output.replace('{{%ID%}}',product.id);
+//         output= output.replace('{{%ROM%}}',product.ROM);
+//         output= output.replace('{{%DESC%}}',product.Description);
     
-        return output;
-}
+//         return output;
+// }
 
 const server = http.createServer((request,response) => {
     let {query,pathname: path} = url.parse(request.url,true);
